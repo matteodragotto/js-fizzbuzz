f0b75c5c6efc8fc1560ad7f27fb7f7dca663cbf6
matteodragotto <matteodrag8@gmail.com>
2024-11-14 14:06:27 +0100

    [feat] basic program logic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 //Scrivi un programma che stampi i numeri da 1 a 100,
 //ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+const fizz = 'Fizz'
+const buzz = 'Buzz'
+
+let normalNumber = 1;
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0)
+    console.log(fizz);
+  else if (i % 5 === 0)
+    console.log(buzz);
+  else if (i % 3) && (i % 5 === 0)
+    console.log(fizz + buzz)
+  else
+    console.log(i)
+     
+    
+    
+}
